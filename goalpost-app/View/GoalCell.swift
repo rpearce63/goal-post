51a18921006c5a046cc3f18b545c68390201b25d
Rick Pearce <rick.pearce.crzs@statefarm.com>
2018-04-06 15:10:15 -0500

    first commit

---
+++ goalpost-app/View/GoalCell.swift
@@ -0,0 +1,23 @@
+//
+//  GoalCell.swift
+//  goalpost-app
+//
+//  Created by Rick Pearce on 4/6/18.
+//  Copyright © 2018 Rick Pearce. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String.init(describing: goalProgressAmount)
+    }
+
+}
