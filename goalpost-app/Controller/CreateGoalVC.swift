843de4954ed523647353b4f8ff68e7b86ece2cb5
Rick Pearce <rick.pearce.crzs@statefarm.com>
2018-04-06 16:53:00 -0500

    bind keyboard and toggle buttons

---
+++ goalpost-app/Controller/CreateGoalVC.swift
@@ -0,0 +1,48 @@
+//
+//  CreateGoalVC.swift
+//  goalpost-app
+//
+//  Created by Rick Pearce on 4/6/18.
+//  Copyright © 2018 Rick Pearce. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shortTermBtn: UIButton!
+    @IBOutlet weak var longTermBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    var goalType : GoalType = .shortTerm
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nextBtn.bindToKeyboard()
+        shortTermBtn.setSelectedColor()
+        longTermBtn.setDeselectedColor()
+        
+    }
+
+    
+    @IBAction func nextBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func shortTermBtnPressed(_ sender: Any) {
+        goalType = .shortTerm
+        shortTermBtn.setSelectedColor()
+        longTermBtn.setDeselectedColor()
+    }
+    
+    @IBAction func longTermBtnPressed(_ sender: Any) {
+        goalType = .longTerm
+        shortTermBtn.setDeselectedColor()
+        longTermBtn.setSelectedColor()
+    }
+    
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismissDetail()
+    }
+}
---
+++ goalpost-app/Enum/GoalType.swift
@@ -0,0 +1,15 @@
+//
+//  GoalType.swift
+//  goalpost-app
+//
+//  Created by Rick Pearce on 4/6/18.
+//  Copyright © 2018 Rick Pearce. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType : String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+    
+}
