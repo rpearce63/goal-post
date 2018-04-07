6490a604bfe0125d431c453357fb4e641c053849
Rick Pearce <rpearce63@yahoo.com>
2018-04-07 09:18:00 -0500

    save data

--- goalpost-app/Controller/FinishGoalVC.swift
+++ goalpost-app/Controller/FinishGoalVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class FinishGoalVC: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var createGoalBtn: UIButton!
@@ -29,11 +30,38 @@ class FinishGoalVC: UIViewController, UITextFieldDelegate {
 
 
     @IBAction func createGoalBtnPressed(_ sender: Any) {
-        // Pass data to Core Data model
+        if pointsTextField.text != "" {
+            self.save { (complete) in
+                if complete {
+                    dismiss(animated: true, completion: nil)
+                }
+            }
+        }
     }
     
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismissDetail()
+    }
     
-
+    func save(completion: (_ finished: Bool) -> ()) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        let goal = Goal(context: managedContext)
+        
+        goal.goalDescription = goalDescription
+        goal.goalType = goalType.rawValue
+        goal.goalCompletionValue = Int32(pointsTextField.text!)!
+        goal.goalProgress = Int32(0)
+        
+        do {
+            try managedContext.save()
+            print("Successfully saved data.")
+            completion(true)
+        } catch {
+            debugPrint("Could not save: \(error.localizedDescription)")
+            completion(false)
+        }
+        
+    }
    
 
 }
